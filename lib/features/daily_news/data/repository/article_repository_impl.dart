0c1dcc22c6a711594a7ec838adaad14752cdcefb
Alan Melendez <alancuevasmelendezalan123@gmail.com>
2026-03-19 23:58:40 -0600

    feat: Implement getNewsArticles method in ArticleRepositoryImpl for fetching news articles

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,17 +1,40 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:news_app_clean_architecture/core/resources/data_state.dart';
 
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 
 import '../../domain/repository/article_repository.dart';
+import '../data_sources/remote/new_api_service.dart';
 
 class ArticleRepositoryImpl extends ArticleRepository {
+  final NewsApiService _newsApiService;
+  ArticleRepositoryImpl(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleEntity>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleEntity>>> getNewsArticles() async {
+    try {
+      final result = await _newsApiService.getNewsArticles(
+        apiKey: DotEnv().env['API_KEY'] ?? '',
+        country: 'us',
+        category: 'technology',
+      );
+
+      if (result.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(result.data);
+      } else {
+        return DataFailed(
+          DioException(
+            requestOptions: result.response.requestOptions,
+            response: result.response,
+            error: 'Failed to fetch news articles',
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
- 
-
-
-
 }
